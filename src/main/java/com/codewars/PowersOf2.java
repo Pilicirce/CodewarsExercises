4ba6d6b10c41877c26db554f4750d5f2a40a70cc
Pilar Solis <pilar.sm02@gmail.com>
2024-05-24 11:47:41 +0200

    add notes in PowersOf2

--- src/main/java/com/codewars/PowersOf2.java
+++ src/main/java/com/codewars/PowersOf2.java
@@ -3,11 +3,16 @@ package com.codewars;
 public class PowersOf2 {
 
     public static long[] powersOfTwo(int n) {
-        
-        long[] result = new long[n + 1];
-        
+
+        long[] result = new long[n + 1]; // inicializa el array con un tamaño de n+1 (para incluir todas ls potencias
+                                         // desde 2^0 hasta 2^n)
+
         for (int i = 0; i <= n; i++) {
+            // Utiliza el método Math.pow de la clase Math para calcular 2^i. Este método devuelve un valor de tipo "double"
+            // (long) convierte el resultado de "double" a "long"
+            // result[i] asigna el calor convertido al elemento "i-ésimo" del array result
             result[i] = (long) Math.pow(2, i);
+
         }
         return result;
     }
--- src/test/java/com/codewars/PowersOf2Test.java
+++ src/test/java/com/codewars/PowersOf2Test.java
@@ -4,11 +4,17 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import org.junit.jupiter.api.Test;
 
 public class PowersOf2Test {
-        @Test
-        public void testSomething() {
-          assertArrayEquals(new long[]{1}, PowersOf2.powersOfTwo(0));
-          assertArrayEquals(new long[]{1,2}, PowersOf2.powersOfTwo(1));
-          assertArrayEquals(new long[]{1,2,4,8,16}, PowersOf2.powersOfTwo(4));
-        }
+  @Test // indica que el método "testSomething" es un test
+  public void testSomething() {
+    // new lon[]{1} define un array esperado que contiene solo un elemento "1" (esto sería la solución esperada)
+    // llamamos al método con el cargumento (0)
+    // AssertArrayEquals: Comprueba que el array resultante de PowersOf2.powersOfTwo(0) es igual al 
+    //array esperado new long[]{1}. Si no son iguales, la prueba falla.
+    assertArrayEquals(new long[] { 1 }, PowersOf2.powersOfTwo(0));
+
+    // Lo mismo con otras pruebas
+    assertArrayEquals(new long[] { 1, 2 }, PowersOf2.powersOfTwo(1));
+    assertArrayEquals(new long[] { 1, 2, 4, 8, 16 }, PowersOf2.powersOfTwo(4));
+  }
 
 }

41be38e4b7d459a1e4bb867b7e9e52e4e12e0fd0
Pilar Solis <pilar.sm02@gmail.com>
2024-05-27 12:52:06 +0200

    add new exercise: RockPapaerScissors

---
+++ src/main/java/com/codewars/RockPaperScissors.java
@@ -0,0 +1,11 @@
+package com.codewars;
+
+public class RockPaperScissors {
+
+    public static String rps(String p1, String p2) {
+        String winner = null;
+       
+        return winner;
+      }
+    
+}

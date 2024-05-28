78d2e90f6114bea19c08224b58775d8e58d20ec6
Pilar Solis <pilar.sm02@gmail.com>
2024-05-28 18:26:36 +0200

    Add MultiplyTheNumber exercise

--- src/main/java/com/codewars/MultiplyTheNumber.java
+++ src/main/java/com/codewars/MultiplyTheNumber.java
@@ -3,7 +3,15 @@ package com.codewars;
 public class MultiplyTheNumber {
     
     public static int multiply(int number) {
-        // Your code here
-        return 0;
+       
+        //convertir el número a una cadena (string) y contar los caracteres.
+        int numberOfDigits = String.valueOf(Math.abs(number)).length();
+
+
+        //para calcular 5 elevado a la potencia del número de dígitos:
+        int result = (int) (number * Math.pow(5, numberOfDigits));
+
+        //devolver el resultado calculado
+        return result;
       }
 }

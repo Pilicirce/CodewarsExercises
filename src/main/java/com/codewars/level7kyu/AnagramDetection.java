6bd7cb6d9776d93904bdb37d61ed7fef73539a5b
Pilar Solis <pilar.sm02@gmail.com>
2024-06-04 07:58:42 +0200

    isAnagram method developed

--- src/main/java/com/codewars/level7kyu/AnagramDetection.java
+++ src/main/java/com/codewars/level7kyu/AnagramDetection.java
@@ -1,10 +1,24 @@
 package com.codewars.level7kyu;
 
+import java.util.Arrays;
+
 public class AnagramDetection {
     public static boolean isAnagram(String test, String original) {
-        // your code goes here
-        return false;
+        
+        // Convertir ambas cadenas a minúsculas
+        String lowerCaseTest = test.toLowerCase();
+        String lowerCaseOriginal = original.toLowerCase();
+
+        // Convertir las cadenas a arrays de caracteres y ordenarlos
+        char[] testArray = lowerCaseTest.toCharArray();
+        Arrays.sort(testArray);
+        String sortedTest = new String(testArray);
+
+        char[] originalArray = lowerCaseOriginal.toCharArray();
+        Arrays.sort(originalArray);
+        String sortedOriginal = new String(originalArray);
+        
+        // Comparar las cadenas ordenadas
+        return sortedTest.equals(sortedOriginal);
       }
-    
-    
 }

f3f579b44708eed6bfe10265a28488d6c0ef5191
Pilar Solis <pilar.sm02@gmail.com>
2024-06-08 14:22:15 +0200

    add TransportationVacation exercise

---
+++ src/main/java/com/codewars/level8kyu/TrasnportationVacation.java
@@ -0,0 +1,9 @@
+package com.codewars.level8kyu;
+
+public class TrasnportationVacation {
+ 
+    public static int rentalCarCost(int d) {
+        // Your solution here
+      }
+    }
+
---
+++ src/test/java/com/codewars/level8kyu/TransportationVacationTest.java
@@ -0,0 +1,29 @@
+package com.codewars.level8kyu;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TransportationVacationTest {
+
+  @Test
+  public void under3Tests() {
+    assertEquals(40, TrasnportationVacation.rentalCarCost(1));
+    assertEquals(80, TrasnportationVacation.rentalCarCost(2));
+  }
+  
+  @Test
+  public void under7Tests() {
+    assertEquals(100, TrasnportationVacation.rentalCarCost(3));
+    assertEquals(140, TrasnportationVacation.rentalCarCost(4));
+    assertEquals(180, TrasnportationVacation.rentalCarCost(5));
+    assertEquals(220, TrasnportationVacation.rentalCarCost(6));
+  }
+  
+  @Test
+  public void over7Tests() {
+    assertEquals(230, TrasnportationVacation.rentalCarCost(7));
+    assertEquals(270, TrasnportationVacation.rentalCarCost(8));
+    assertEquals(310, TrasnportationVacation.rentalCarCost(9));
+    assertEquals(350, TrasnportationVacation.rentalCarCost(10));
+  }
+}
+

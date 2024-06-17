2ef748a5b5d4278354339f5e5109bd8dd9ca2c68
Pilar Solis <pilar.sm02@gmail.com>
2024-06-17 07:55:46 +0200

    add code for GrowthPopulation exercise

--- src/main/java/com/codewars/level7kyu/GrowthPopulation.java
+++ src/main/java/com/codewars/level7kyu/GrowthPopulation.java
@@ -1,5 +1,18 @@
 package com.codewars.level7kyu;
 
 public class GrowthPopulation {
+
+    public static int nbYear(int p0, double percent, int aug, int p) {
+        int years = 0;
+        
+        while (p0 < p) {
+            p0 += p0* (percent / 100) + aug;
+            years++;
+        }
+        
+        return years;
+    }
+        
+ }
     
-}
+
--- src/test/java/com/codewars/level7kyu/GrowthPopulationTest.java
+++ src/test/java/com/codewars/level7kyu/GrowthPopulationTest.java
@@ -1,5 +1,17 @@
 package com.codewars.level7kyu;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
 public class GrowthPopulationTest {
-    
+    private static void testing(int actual, int expected) {
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void test1() {
+        System.out.println("Fixed Tests: nbYear");    
+        testing(GrowthPopulation.nbYear(1500, 5, 100, 5000),15);
+        testing(GrowthPopulation.nbYear(1500000, 2.5, 10000, 2000000), 10);
+        testing(GrowthPopulation.nbYear(1500000, 0.25, 1000, 2000000), 94);
+    }
 }

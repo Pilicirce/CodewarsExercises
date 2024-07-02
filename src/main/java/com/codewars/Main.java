19dd5bdcdd10fbdbe33b657888ceb12ad0bd00e1
Pilar Solis <pilar.sm02@gmail.com>
2024-07-02 12:41:32 +0200

    add SumNoDuplicates classes

--- src/main/java/com/codewars/Main.java
+++ src/main/java/com/codewars/Main.java
@@ -190,6 +190,13 @@ public class Main {
 
         int years = GrowthPopulation.nbYear(1500, 5, 100, 5000);
         System.out.println("Years needed: " + years);
+
+
+
+        //Ejercicio 11: Sum a list but ignore any duplicates
+        //DESCRIPTION: Please write a function that sums a list, but ignores any duplicated items in the list.
+        //For instance, for the list [3, 4, 3, 6] the function should return 10, 
+        //and for the list [1, 10, 3, 10, 10] the function should return 4.
         
 
 

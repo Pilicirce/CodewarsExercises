b07619d1fd7b138c863a77bc1f9ef5dd735fe300
Pilar Solis <pilar.sm02@gmail.com>
2024-07-09 13:00:33 +0200

    add execise 12 description in Main

--- src/main/java/com/codewars/Main.java
+++ src/main/java/com/codewars/Main.java
@@ -213,6 +213,20 @@ public class Main {
         System.out.println("Resultado para list1: " + resultList1); // Debería imprimir 5
         System.out.println("Resultado para list2: " + resultList2); // Debería imprimir 6
         System.out.println("Resultado para list3: " + resultList3); // Debería imprimir 3
+
+
+
+
+        //Ejercicio 12: Count the divisors of a number
+        //DESCRIPTION: Count the number of divisors of a positive integer n.
+        //Random tests go up to n = 500000. 
+        //Examples (input --> output)
+        //4 --> 3 // we have 3 divisors - 1, 2 and 4
+        //5 --> 2 // we have 2 divisors - 1 and 5
+        //12 --> 6 // we have 6 divisors - 1, 2, 3, 4, 6 and 12
+        //30 --> 8 // we have 8 divisors - 1, 2, 3, 5, 6, 10, 15 and 30
+        //Note you should only return a number, the count of divisors. The numbers between parentheses are shown 
+        //only for you to see which numbers are counted in each case.
     
 
 

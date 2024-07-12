e2789615cb6cec9e5efa6c0a1e1a9ecfbfd91044
Pilar Solis <pilar.sm02@gmail.com>
2024-07-12 12:16:29 +0200

    add code for Ex12_FindDivisor

--- src/main/java/com/codewars/level7kyu/Ex12_FindDivisor.java
+++ src/main/java/com/codewars/level7kyu/Ex12_FindDivisor.java
@@ -7,20 +7,14 @@ public class Ex12_FindDivisor {
     // Inicializa el contador de divisores
     int count = 0;
 
-    // Itera desde 1 hasta la raíz cuadrada de n
-    for (int i = 1; i <= Math.sqrt(n); i++) {
+    // Iterar desde 1 hasta n
+    for (int i = 1; i <= n; i++) {
       // Si i es divisor de n
       if (n % i == 0) {
-          // Incrementa el contador
           count++;
-          // Verifica y cuenta el complemento n / i si es diferente de i
-          if (i != n / i) {
-              count++;
-          }
-
-        }
       }
-      
+  }
+  
       // Retorna el número total de divisores
       return count;
 
